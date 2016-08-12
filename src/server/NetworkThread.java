f924bbe0a5bc0f5b40214644ca58f40efe776e7a
Sudipto Bhattacharjee <ttsudipto@gmail.com>
2016-08-12 22:41:44 +0530

    Remove not required updateParameters() method

--- src/server/NetworkThread.java
+++ src/server/NetworkThread.java
@@ -5,36 +5,15 @@ package server;
  */
 public class NetworkThread implements Runnable {
     private NetworkManager manager;
-    private static boolean doIt, option;
 
     public NetworkThread(NetworkManager manager) {
         this.manager = manager;
-        doIt = false;
-    }
-
-    public void updateParameters(boolean doIt, boolean option) {
-        this.doIt = doIt;
-        this.option = option;
-        System.out.println(this.doIt + " " + this.option);
     }
 
     @Override
     public void run() {
         try {
-//            while (true) {
-////                System.out.println(doIt + " " + option);
-//                if (option == true && doIt == true){
-//                    doIt = false;
-                    manager.startServer();
-//                    Thread.currentThread().suspend();
-//                }
-//                if (option == false && doIt == true) {
-//                    System.out.println("foo");
-//                    doIt = false;
-////                    manager.stopServer();
-//                    break;
-//                }
-//            }
+                manager.startServer();
         } catch (Exception e) { e.printStackTrace(); }
     }
 }
